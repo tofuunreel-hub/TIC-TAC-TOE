e5844ba346b63681ac42af94b78a988467cb8e4e
Emma-Rose Soubeste <pret@LAPTOP-0SRF8ODV>
2025-11-13 15:41:05 +0100

    Ajout des clicks en js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 console.log('Hello world !');
+
+let player1EL = document.querySelector('#player1');
+let player2EL = document.querySelector('#player2');
+
+let player1ItemEL = document.querySelector('#player1 .item');
+let player1NameEL = document.querySelector('#player1 .name');
+let player1ScoreEL = document.querySelector('#player1 .score');
+
+let player2ItemEL = document.querySelector('#player2 .item');
+let player2NameEL = document.querySelector('#player2 .name');
+let player2ScoreEL = document.querySelector('#player2 .score');
+
+let msgEL = document.querySelector('#msg');
+let gridEL = document.querySelector('.grid');
+
+player1ItemEL.innerHTML = 'X';
+player1ScoreEL.innerHTML = '0';
+player1NameEL.innerHTML = 'Joueur 1';
+
+player2ScoreEL.innerHTML = '0';
+player2ScoreEL.innerHTML = '0';
+player2NameEL.innerHTML = 'Joueur 2';
